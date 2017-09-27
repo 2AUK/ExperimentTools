a438271a46f45f09830b4127e3fd207b291e55e6
Abdullah Ahmad <ahmada1uk@googlemail.com>
2017-09-27 13:26:50 +0100

    Added writing and duplicate removal functions

--- automate.py
+++ automate.py
@@ -18,7 +18,7 @@ for pid in FOLDERS:
     for auto in FOLDERS:
         generated_file = (ROOT + "/" + auto + "/" + "6-Comparison/O.pdb")
         ofile = open(OUTPUT + "/" + pid + "/" +"Ref_" + pid + "_Gen_" + auto + ".txt", 'w')
-        ofile.write("Crystal ID\tPredicted ID\tDistance\n")
+        ofile.write("Crystal ID\tPredicted ID\tDensity\tDistance\n")
         with open(current_ref, "r") as exp_file:
             for line in exp_file:
                 if re.match('(.*)HOH(.*)', line):
@@ -36,6 +36,7 @@ for pid in FOLDERS:
                                 y_2 = float(el[6])
                                 z_2 = float(el[7])
                                 Pred_ID = el[1]
+                                Density = el[8]
 
                                 distance = float(math.sqrt((float(x_1 - x_2) ** 2) + \
                                     (float(y_1 - y_2) ** 2) + \
@@ -44,5 +45,28 @@ for pid in FOLDERS:
                                 if distance <= CUTOFF:
                                     ofile.write(Exp_ID + "\t\t")
                                     ofile.write(Pred_ID + "\t\t")
+                                    ofile.write(Density + "\t\t")
                                     ofile.write(str(distance) + "\n")
+
 ofile.close()
+def remove_duplicates(elist, plist, denlist, dlist):
+    """
+    Remove any duplicates from the data
+    """
+    data = (zip(elist, plist, denlist, dlist))
+    duplicate_df = pd.DataFrame(data, columns=["Experimental ID", "Predicted ID",\
+    "Density", "Distance"])
+    duplicate_df.sort_values("Distance", inplace=True, ascending=False)
+    duplicate_df.drop_duplicates(subset=["Conserved ID"], keep="last", inplace=True)
+    return duplicate_df["Conserved ID"].tolist(), duplicate_df["Reference ID"].tolist(), \
+    duplicate_df["Density"].tolist(), duplicate_df["Distance"].tolist()
+
+def write_text(filewrite, elist, plist, denlist, dlist):
+    """
+    Writing all the text files in a convenient and contained manner.
+    """
+    for i in zip(elist, plis, denlist, dlist):
+        filewrite.write(i[0])
+        filewrite.write(i[1])
+        filewrite.write(i[2])
+        filewrite.write(i[3])
